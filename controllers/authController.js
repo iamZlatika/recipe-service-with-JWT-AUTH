23631f23b9e7b91233d6af3482812bcdf7b2e501
iamZlatika <mazurovazv@gmail.com>
2020-08-20 18:09:40 +0300

    Create validation. Make system of processing errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,25 @@
 const User = require('../models/User')
 
+
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: '', password: '' };
+
+  if (err.code === 11000){
+    errors.email = 'This email is already registered';
+    return errors;
+  }
+
+
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message
+    })
+  }
+  return errors
+}
+
+
 module.exports.signup_get = (req, res) => {
   res.render('signup')
 }
@@ -10,13 +30,13 @@ module.exports.login_get = (req, res) => {
 
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
-  
+
   try {
     const user = await User.create({ email, password });
     res.status(201).json(user)
   } catch (err) {
-    console.log(err);
-    res.status(400).send('error, user is not created')
+    const errors = handleErrors(err);
+    res.status(400).json({ errors })
   }
 }
 
