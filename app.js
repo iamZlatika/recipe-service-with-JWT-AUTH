144f0faa9464d08f82fb8e9affeb12d5f8303615
iamZlatika <mazurovazv@gmail.com>
2020-08-21 16:16:41 +0300

    Work with cookies. Create own cookies with props

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
 const dbURI = 'mongodb+srv://jwtKeeper:test1234@contactkeeper.9hm5l.mongodb.net/jwt';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
@@ -22,3 +24,21 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 
 app.use(authRoutes);
+
+// cookies hhh
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
+
+  res.send('you got the cookie')
+});
+
+
+app.get('/read-cookies', (req, res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+  res.json(cookies)
+
+});
